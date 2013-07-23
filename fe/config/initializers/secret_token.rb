384808ba0510f093883263654da88788b0c24be6
roxion1377 <roxion1377@gmail.com>
2013-07-23 21:01:27 +0900

    iroiro

--- fe/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Judge::Application.config.secret_token = '4bb3ca912e94c11a9bd1d8f5dd9a09bccbde109eeef5aa49c53737a5c9323e075b467289ce3079642be2cab8e0a71100f9aaebd94d060c404e4009898d9d2932'
